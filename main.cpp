74973b59b127a24f15441dedc5540f6a09874c46
Dhjali <barbara.vanni@laplateforme.io>
2024-09-02 16:06:46 +0200

    [MODIFY] create board object and display it on terminal

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "game/logic_game/HPP_files/Board.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // creation of the board
+    Board board;
+    // seed for random number generation base on the actual hour 
+    srand(static_cast<unsigned int>(time(0)));
+
+    board.boardInit();
+    board.displayBoard();
+
+
     return 0;
 }
