27b785dadff5f92631252f174efbadb0352148db
mathis-serra <mathis.serra@laplateforme.io>
2024-09-02 22:06:32 +0200

    [TRYING] Trying to create the board on screen

--- game/logic_game/CPP_files/Tiles.cpp
+++ game/logic_game/CPP_files/Tiles.cpp
@@ -1,32 +1,57 @@
-#ifndef TILES_HPP
-#define TILES_HPP
-
+#include "../HPP_files/Tiles.hpp"
 #include <iostream>
-class Tiles {
-    private:
-    int tileHeight = 145;
-    int tileWidth = 145;
-    int posX;
-    int posY;
-    int numberInTile;
 
-public:
-    Tiles(int posX, int posY, int numberInTile);
-    ~Tiles();
+/*
+    This class is responsible for creating the tiles that will be used in the game.
+    Each tile has a position in the game board and a number that will be used to merge with other tiles.
+    The tiles can be merged if they have the same number.
+*/
 
-    // Getters
-    int getPosX();
-    int getPosY();
-    int getNumberInTile();
+// Constructor & Destructor
+Tiles::Tiles(int posX, int posY, int numberInTile) {
+    this->posX = posX;
+    this->posY = posY;
+    this->numberInTile = numberInTile;
+}
 
-    // Setters
-    void setPosX(int posX);
-    void setPosY(int posY);
-    void setNumberInTile(int numberInTile);
+Tiles::~Tiles() {
+    std::cout << "Destructor called" << std::endl;
+}
 
-    // Other methods
-    int mergeTilesNumbers(Tiles* tile);
+// Getters
+int Tiles::getPosX() {
+    return posX;
+}
 
-};
+int Tiles::getPosY() {
+    return posY;
+}
 
-#endif
+int Tiles::getNumberInTile() {
+    return numberInTile;
+}
+
+// Setters
+void Tiles::setPosX(int posX) {
+    this->posX = posX;
+}
+
+void Tiles::setPosY(int posY) {
+    this->posY = posY;
+}
+
+void Tiles::setNumberInTile(int numberInTile) {
+    this->numberInTile = numberInTile;
+}
+
+// Other methods
+int Tiles::mergeTilesNumbers(Tiles* tile) {
+    if (this->numberInTile == tile->getNumberInTile()) {
+        this->numberInTile += tile->getNumberInTile();
+        tile->setNumberInTile(0);  
+        return this->numberInTile;
+    } else {
+        std::cout << "Tiles cannot merged" << std::endl;
+        return this->numberInTile; 
+    }
+}
