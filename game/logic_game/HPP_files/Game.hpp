eaf14a747fa2ea2a98c659a2988d64acdac8fa89
Dhjali <barbara.vanni@laplateforme.io>
2024-09-03 10:27:05 +0200

    [MODIFY] game class // handle all the game logic

--- game/logic_game/HPP_files/Game.hpp
+++ game/logic_game/HPP_files/Game.hpp
@@ -2,5 +2,25 @@
 #define GAME_HPP
 
 #include <iostream>
+#include "Board.hpp"
+
+class Game {
+    public:
+        Game();
+        ~Game();
+
+        void start();
+        void move();
+        bool checkDefeat();
+        bool checkVictory();
+        void displayScore();
+
+    private:
+        Board* board;
+        int score;
+        bool gameOver;
+};
+
+
 
 #endif // GAME_HPP
