a2ae331f01eecf9f878afd88889b2a413fac7c98
Dhjali <barbara.vanni@laplateforme.io>
2024-09-04 15:19:37 +0200

    [ADD] button class

---
+++ game/graphic_game/SFML/HPP_files/Button.hpp
@@ -0,0 +1,26 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+
+#include <iostream>
+
+
+class Button{
+public:
+//     Button (sf::Image* normal,sf::Image* clicked,std::string,sf::Vector2f location);
+//     ~Button();
+//     void checkClick (sf::Vector2f);
+//     void setState(bool);
+//     void setText(std::string);
+//     bool getVar();
+//     sf::Sprite* getSprite();
+//     sf::String * getText();
+// private:
+//     sf::Sprite normal;
+//     sf::Sprite clicked;
+//     sf::Sprite* currentSpr;
+//     sf::String String;
+//     bool current;
+};
+
+
+#endif // BUTTON_HPP
