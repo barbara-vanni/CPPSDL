e42569fa1b36e06e854d04121bdbdda71a6cccc0
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-09-07 11:01:52 +0200

    [UPDATE] background grid done in GridSfml::drawGrid. we will have to change it later and add it on a Background class

--- game/graphic_game/SFML/CPP_files/GridSfml.cpp
+++ game/graphic_game/SFML/CPP_files/GridSfml.cpp
@@ -22,18 +22,37 @@ double GridSfml::height() {
     return gridSize;
 }
 
+// void GridSfml::drawGrid(sf::RenderWindow* window) {
+//     sf::RectangleShape rectangle(sf::Vector2f(gridSize, gridSize));
+//     auto& grid = game.getBoard()->getGrid();
+//     for (int i = 0; i < 4; ++i) {
+//         for (int j = 0; j < 4; ++j) {
+//             if (grid[i][j] != nullptr && grid[i][j]->getNumberInTile() != 0) {
+//                 TilesSfml tileSfml(game);
+//                 // std::cout << "Drawing tile at (" << i << ", " << j << ") with value " << grid[i][j]->getNumberInTile() << std::endl;  // Debugging
+//                 tileSfml.drawTile(window, grid[i][j], gridPosX, gridPosY, gridSize);
+//             }
+//         }
+//     }
+// }
+
 void GridSfml::drawGrid(sf::RenderWindow* window) {
-    sf::RectangleShape rectangle(sf::Vector2f(gridSize, gridSize));
+
+    sf::Color gridColor = sf::Color(140, 140, 140); 
+    sf::RectangleShape gridBackground(sf::Vector2f(gridSize, gridSize));
+    gridBackground.setPosition(gridPosX, gridPosY);
+    gridBackground.setFillColor(gridColor);
+    window->draw(gridBackground);
+
     auto& grid = game.getBoard()->getGrid();
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
             if (grid[i][j] != nullptr && grid[i][j]->getNumberInTile() != 0) {
                 TilesSfml tileSfml(game);
-                // std::cout << "Drawing tile at (" << i << ", " << j << ") with value " << grid[i][j]->getNumberInTile() << std::endl;  // Debugging
                 tileSfml.drawTile(window, grid[i][j], gridPosX, gridPosY, gridSize);
             }
         }
     }
 }
-            //     rectangle.setFillColor(sf::Color(140, 140, 140, 255));
-            //     window->draw(rectangle);
+
+
