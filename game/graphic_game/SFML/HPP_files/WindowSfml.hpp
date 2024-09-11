01b087f860b8d2363a27b3a35f822e9d36ecff7c
mathis-serra <mathis.serra@laplateforme.io>
2024-09-11 13:28:16 +0200

    Unquotes WindowSFML

--- game/graphic_game/SFML/HPP_files/WindowSfml.hpp
+++ game/graphic_game/SFML/HPP_files/WindowSfml.hpp
@@ -1,26 +1,26 @@
-// #ifndef WindowSFML_HPP
-// #define WindowSFML_HPP
-// #include "../../src/include/SFML/Graphics.hpp"
-// #include "../../src/include/SFML/Window/Event.hpp"
-// #include <iostream>
-// #include <memory>
+#ifndef WindowSFML_HPP
+#define WindowSFML_HPP
+#include "../../src/include/SFML/Graphics.hpp"
+#include "../../src/include/SFML/Window/Event.hpp"
+#include <iostream>
+#include <memory>
 
-// class WindowSfml {
-//     private: 
-//         bool init();
-//         std::unique_ptr<sf::RenderWindow> Window;
-//         std::unique_ptr<sf::RectangleShape> rectangle;
-//         int width;
-//         int height;
-//         std::string title;
-//         bool closed;
+class WindowSfml {
+    private: 
+        bool init();
+        std::unique_ptr<sf::RenderWindow> Window;
+        std::unique_ptr<sf::RectangleShape> rectangle;
+        int width;
+        int height;
+        std::string title;
+        bool closed;
 
-//     public:
-//     WindowSfml(int width = 600, int height = 800);
-//     ~WindowSfml() = default; 
-//     void clear() const;
-//     bool isClosed();
-//     sf::RenderWindow* getWindowSfml() const;
-// };
+    public:
+    WindowSfml(int width = 600, int height = 800);
+    ~WindowSfml() = default; 
+    void clear() const;
+    bool isClosed();
+    sf::RenderWindow* getWindowSfml() const;
+};
 
-// #endif 
+#endif 
